24f7c6cca942b16ea9a45f551b418eb67a7c081f
Alex Smith <alex.geoffrey.smith@gmail.com>
2014-10-25 23:31:06 +1100

    Clean up test file uploads after suite.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,13 @@ $LOAD_PATH << File.expand_path('../../app', __FILE__)
 ENV["RACK_ENV"] = "test"
 
 require 'config/environment'
+
+RSpec.configure do |config|
+  config.after(:suite) do
+    # Clean up CarrierWave uploads.
+    if defined?(CarrierWave)
+      uploads_dir = "#{CarrierWave::Uploader::Base.root}/uploads"
+      FileUtils.rm_rf(uploads_dir)
+    end
+  end
+end
