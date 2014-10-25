d0587185619dbd37120f84669a8374d3333096e5
Alex Smith <alex.geoffrey.smith@gmail.com>
2014-10-25 23:37:16 +1100

    Bootstrap script checks for foreman installation.
    foreman is not included in the Gemfile as per the intended usage of
    foreman - to be a tool outside the application.

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -9,3 +9,9 @@ bundle install
 createuser -S -d puppy_love
 createdb --owner=puppy_love puppy_love
 createdb --owner=puppy_love puppy_love_test
+
+# Print warning if Foreman is not installed.
+if ! command -v foreman &>/dev/null; then
+  echo "foreman is not installed."
+  echo "See https://github.com/ddollar/foreman for install instructions."
+fi
