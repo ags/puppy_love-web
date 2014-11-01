298fac5779f5103146d907ea295225232ad75e00
Alex Smith <alex.geoffrey.smith@gmail.com>
2014-11-01 16:09:28 +1100

    Provide resource type as part of schema.

--- app/serializers/dog_serializer.rb
+++ app/serializers/dog_serializer.rb
@@ -6,7 +6,9 @@ class DogSerializer < Oat::Serializer
   adapter Oat::Adapters::HAL
 
   schema do
-    link :self, href: "/api/dogs/#{item.id}"
+    link :self,
+      href: "/api/dogs/#{item.id}",
+      type: "dog"
 
     properties do |props|
       props.name item.name
